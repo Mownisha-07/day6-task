e867c368aab8ce2c2ec07162cd493f78d94b34ea
Mownisha-07 <mownisha2004@gmail.com>
2024-07-10 15:25:10 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,18 @@
+var num = Math.floor(Math.random() * 101);
+var count = 0;
+console.log(num);
+
+function findnum() {
+  var guessed_num = parseInt(document.getElementById("n1").value);
+  count += 1;
+
+  if (guessed_num == num) {
+    document.getElementById("result").textContent =
+      "Wow!!Congratulation!!You find the number";
+  } else if (guessed_num > num) {
+    document.getElementById("result").textContent = "Try with smaller number";
+  } else {
+    document.getElementById("result").textContent = "Try with greater number";
+  }
+  document.getElementById("count").textContent = count;
+}
